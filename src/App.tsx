9af53f4fa5021f0db35229c2bb872e97ca5f2699
letofftv <letoff.art@gmail.com>
2026-05-06 09:50:55 +0300

    Restore all routes but keep HelmetProvider removed to check if it fixes the blank screen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
 import { PageLayout } from './components/layout/PageLayout';
 import { Home } from './pages/Home';
 import { Excursions } from './pages/Excursions';
@@ -20,9 +19,26 @@ import { BookingRules } from './pages/legal/BookingRules';
 
 export default function App() {
   return (
-    <div style={{ padding: '100px', textAlign: 'center', fontSize: '24px' }}>
-      <h1>RusinTravel Debug</h1>
-      <p>Если вы видите этот текст, значит React работает.</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PageLayout />}>
+          <Route index element={<Home />} />
+          <Route path="ekskursii" element={<Excursions />} />
+          <Route path="ekskursii/:slug" element={<ExcursionDetail />} />
+          <Route path="obo-mne" element={<About />} />
+          <Route path="galereya" element={<Gallery />} />
+          <Route path="events" element={<Events />} />
+          <Route path="events/:slug" element={<EventDetail />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="blog/:slug" element={<BlogPostPage />} />
+          <Route path="kontakty" element={<Contacts />} />
+          <Route path="legal/privacy" element={<PrivacyPolicy />} />
+          <Route path="legal/offer" element={<Offer />} />
+          <Route path="legal/personal-data" element={<PersonalDataConsent />} />
+          <Route path="legal/booking-rules" element={<BookingRules />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
